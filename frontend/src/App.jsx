5c6b25e0ec6a435e27cbbc0e5c8c2e9be5210952
IC <ilijana.covic@fsre.sum.ba>
2026-03-20 10:41:33 +0100

    Dodan Navbar s navigacijom

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,12 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import LoginPage from './pages/LoginPage'
 import RegisterPage from './pages/RegisterPage'
+import Navbar from './components/Navbar'
 
 function App() {
   return (
     <BrowserRouter>
+    <Navbar />
       <Routes>
         <Route path="/" element={<div>Home</div>} />
          <Route path="/login" element={<LoginPage />} />

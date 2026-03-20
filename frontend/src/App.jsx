04d9e3780dfb695df911551dcfa8ba386a482268
IC <ilijana.covic@fsre.sum.ba>
2026-03-20 14:33:59 +0100

    Dodana početna stranica – naslov, pretraga, kategorije i prikaz usluga

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,7 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import LoginPage from './pages/LoginPage'
 import RegisterPage from './pages/RegisterPage'
+import HomePage from './pages/HomePage'
 import Navbar from './components/Navbar'
 
 function App() {
@@ -8,7 +9,7 @@ function App() {
     <BrowserRouter>
     <Navbar />
       <Routes>
-        <Route path="/" element={<div>Home</div>} />
+        <Route path="/" element={<HomePage />} />
          <Route path="/login" element={<LoginPage />} />
          <Route path="/register" element={<RegisterPage />} />
       </Routes>

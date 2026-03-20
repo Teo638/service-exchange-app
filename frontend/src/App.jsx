93d675b9f516531fbd71c6d192b1ab0c4c61fbe6
IC <ilijana.covic@fsre.sum.ba>
2026-03-20 15:59:43 +0100

    Dodana stranica s detaljima usluge

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,6 +2,7 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import LoginPage from './pages/LoginPage'
 import RegisterPage from './pages/RegisterPage'
 import HomePage from './pages/HomePage'
+import ServiceDetailsPage from './pages/ServiceDetailsPage'
 import Navbar from './components/Navbar'
 
 function App() {
@@ -12,6 +13,7 @@ function App() {
         <Route path="/" element={<HomePage />} />
          <Route path="/login" element={<LoginPage />} />
          <Route path="/register" element={<RegisterPage />} />
+          <Route path="/services/:id" element={<ServiceDetailsPage />} />
       </Routes>
     </BrowserRouter>
   )

ab9b4f834a25dd41c6add575d1523d70942805fc
Teo <119760314+Teo638@users.noreply.github.com>
2026-03-26 17:01:57 +0100

    Implementiran brojac neprocitanih poruka i markAsRead ruta

--- backend/src/routes/messageRoutes.js
+++ backend/src/routes/messageRoutes.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const router = express.Router();
-const { sendMessage, getChatHistory, getContacts } = require('../controllers/messageController');
+const { sendMessage, getChatHistory, getContacts, markAsRead } = require('../controllers/messageController');
 const auth = require('../middlewares/authMiddleware');
 
 router.post('/', auth, sendMessage);
 router.get('/contacts', auth, getContacts);
 router.get('/:userId', auth, getChatHistory);
+router.put('/read/:senderId', auth, markAsRead);
 
 module.exports = router;
